9c78f06bf339b053738f50b3ea1399b4894b964c
Sarvesh <sarvesh1692005@gmail.com>
2026-09-05 10:29:07 +0530

    Add Lambda deployment and portfolio evidence

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -432,13 +432,7 @@ def lambda_handler(event, context):
         )
     )
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            report,
-            default=str,
-        ),
-    }
+    return report
 
 
 if __name__ == "__main__":
